77756ed52b6a69b251b16104910e01fed7361347
kevinBravo <kbravo1024@gmail.com>
2024-01-28 16:05:41 -0800

    add interest calculator

--- small_project/client/src/components/App.js
+++ small_project/client/src/components/App.js
@@ -1,8 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [total, setTotal] = useState([])
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const interestForm= Object.fromEntries(new FormData(event.target).entries());
+    
+    setTotal(parseFloat(interestForm.amount) + parseFloat(interestForm.amount*(interestForm.rate/100)))
+  }
+
+  return (
+    <div>
+      <h1>Project Client</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Please enter the amount owed: </label>
+        <input type="text" name="amount"></input>
+        <br></br>
+        <label>Please enter the interest rate: </label>
+        <input type="text" name="rate"></input>
+        <br></br>
+        <button type="submit">Amount due:</button>
+      </form>
+      <p>{total}</p>
+    </div>
+    )
 }
 
 export default App;
--- small_project/server/app.py
+++ small_project/server/app.py
@@ -1,23 +1,55 @@
-#!/usr/bin/env python3
-
-# Standard library imports
-
-# Remote library imports
-from flask import request
-from flask_restful import Resource
-
-# Local imports
-from config import app, db, api
-# Add your model imports
+# #!/usr/bin/env python3
 
 
-# Views go here!
+# # Standard library imports
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+# # Remote library imports
+# from flask import request
+# from flask_restful import Resource
+
+# # Local imports
+# from config import app, db, api
+# # Add your model imports
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+# # Views go here!
 
+# @app.route('/')
+# def index():
+#     return '<h1>Project Server</h1>'
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
+#method create staircase
+def create_staircase(filename):
+    with open(filename, 'r') as file:
+        file_lines = [line for line in file]
+    # Sort the list in ascending order
+    sorted_file_list = sorted(file_lines, key = lambda x: int(x.split(' ')[0]))
+
+    step = 1
+    subsets = []
+    while len(sorted_file_list) != 0:
+        if len(sorted_file_list) >= step:
+            subsets.append(sorted_file_list[0:step])
+            sorted_file_list = sorted_file_list[step:]
+            step += 1
+        else:
+            break
+
+    return subsets
+def decode(message_file):
+    pyramid = create_staircase(message_file)
+    code = []
+    for ele in pyramid:
+        if len(ele) == 1:
+            code.append((ele[0].split(' ',1)[1]).replace('\n',''))
+        else:
+            code.append((ele[-1].split(' ',1)[1]).replace('\n',''))
+    sentence = ' '.join(code)
+
+    return sentence
+print(decode('filr.txt'))
