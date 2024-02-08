bf5c92fd23d8aa839112624ace1b9b4a716aa44a
kevinBravo <kbravo1024@gmail.com>
2024-02-07 16:40:35 -0800

    modify equation

--- small_project/client/src/components/App.js
+++ small_project/client/src/components/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 
 function App() {
-  const [total, setTotal] = useState([])
+  const [total, setTotal] = useState([]);
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
-    const interestForm= Object.fromEntries(new FormData(event.target).entries());
-    
-    setTotal(parseFloat(interestForm.amount) * (1+(parseFloat((interestForm.rate/100)))))
+    const interestForm = Object.fromEntries(
+      new FormData(event.target).entries()
+    );
+    var rateDec = interestForm.rate/100
+    var compoundInterest = interestForm.amount * Math.pow((1 + rateDec/ 12), (12 * interestForm.years))
+    setTotal(compoundInterest)
   }
 
   return (
@@ -20,11 +23,15 @@ function App() {
         <label>Please enter the interest rate: </label>
         <input type="text" name="rate"></input>
         <br></br>
+        <label>Please enter the duartion of the payments:</label>
+        <input type="text" name="years" placeholder="years"></input>
+        <br></br>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
         <button type="submit">Amount due:</button>
       </form>
+      <p>Total owed after duration compunded monthly for x amounbt of years</p>
       <p>{total}</p>
     </div>
-    )
+  );
 }
 
 export default App;
